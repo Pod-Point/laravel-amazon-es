969ed59e4c43ae0bf51b3dcbfc87bf81e60627a4
threesquared <ben@3sq.re>
2017-10-18 17:10:46 +0100

    Begin ES package

---
+++ src/Providers/ElasticsearchProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace PodPoint\LaravelAmazonElasticsearch\Providers;
+
+use Elasticsearch\Client;
+use Illuminate\Support\ServiceProvider;
+use Aws\Credentials\CredentialProvider;
+use Aws\Credentials\Credentials;
+use Elasticsearch\ClientBuilder;
+use Aws\ElasticsearchService\ElasticsearchPhpHandler;
+
+class ElasticsearchProvider extends ServiceProvider
+{
+    /**
+     * Create a new ES client that uses signed AWS credentials.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->singleton(Client::class, function () {
+            $client = ClientBuilder::create();
+
+            $provider = CredentialProvider::fromCredentials(
+                new Credentials(env('AWS_KEY'), env('AWS_SECRET'))
+            );
+
+            $handler = new ElasticsearchPhpHandler(env('AWS_REGION', 'eu-west-1'), $provider);
+
+            return $client->setHandler($handler)->build();
+        });
+    }
+}
